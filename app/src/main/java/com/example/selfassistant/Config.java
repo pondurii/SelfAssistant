1937f649becf63c071811bdc62c09fd4a6b3211b
ponduri <brahmam.cmrit@gmail.com>
2017-02-10 17:24:48 +0530

    add some commands to test app

--- app/src/main/java/com/example/selfassistant/Config.java
+++ app/src/main/java/com/example/selfassistant/Config.java
@@ -1,32 +1,39 @@
 package com.example.selfassistant;
 
 /***********************************************************************************************************************
- *
  * API.AI Android SDK -  API.AI libraries usage example
  * =================================================
- *
+ * <p>
  * Copyright (C) 2014 by Speaktoit, Inc. (https://www.speaktoit.com)
  * https://www.api.ai
- *
- ***********************************************************************************************************************
- *
+ * <p>
+ * **********************************************************************************************************************
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- *
  ***********************************************************************************************************************/
 
 public abstract class Config {
     // copy this keys from your developer dashboard
-   // public static final String ACCESS_TOKEN = "3485a96fb27744db83e78b8c4bc9e7b7";
+    // public static final String ACCESS_TOKEN = "3485a96fb27744db83e78b8c4bc9e7b7";
 
     // 1. get ACCESS_TOKEN from https://api.ai/
     // 2. create some intents at https://api.ai/ under agent.
+
+    //3.use the following commands to check
+    //Hi
+    //who are you?
+    //how are you
+    //about india
+    //any joke
+    //calender
+    //current location
     public static final String ACCESS_TOKEN = "c0c24ffd06ac4e60a735e41a747b50e8";
 
 
